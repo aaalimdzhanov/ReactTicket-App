22e8e287067a94cd8f1831f11eb26e95cc76165f
Asomiddin <aaaalimdzhanov@beelab.uz>
2019-09-05 12:42:14 +0500

    create project structure

---
+++ src/service/ticket-service.js
@@ -0,0 +1,25 @@
+export default class TicketService{
+    _apiBase = 'http://localhost:8082/ticket';
+    async getResource(url){
+        const res = await fetch(`${this._apiBase}${url}`);
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}` , `receved ${res.status}`)
+        }
+        return await res.json();
+    }
+
+    async getAllTicket(){
+        const res = await this.getResource(`/all`);
+        return res.result;
+    }
+    getTicketById(id){
+        return this.getResource(`/getTicketById/{id}`)
+    }
+}
+
+const ticketService = new TicketService();
+ticketService.getAllTicket().then((ticket)=>{
+    ticket.forEach((t)=>{
+        console.log(t.id);
+    })
+});
