7b513a7906e24efeba745b8a11320460d491b102
Asomiddin <aaaalimdzhanov@beelab.uz>
2019-09-18 13:19:37 +0500

    change header

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import Header from '../header/header'
+
+
+export default class App extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div className="conatiner-scroller">
+                <Header />
+            </div>
+          );
+    }
+}
+ 
